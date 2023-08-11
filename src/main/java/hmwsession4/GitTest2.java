ba65f7d593ed63f67ee14e0d1abd39cd25075137
Ayman Mreiden <amoryden@gmail.com>
2023-08-11 11:04:43 -0500

    please review

---
+++ src/main/java/hmwsession4/GitTest2.java
@@ -0,0 +1,23 @@
+package hmwsession4;
+
+public class GitTest2 {
+
+	public static void main(String[] args) {
+
+		System.out.println("git test");
+		printMe();
+
+	}
+
+	public static void printMe() {
+
+		System.out.println("git test 1");
+		System.out.println("git test 2");
+		System.out.println("git test 3");
+		System.out.println("git test 4");
+		System.out.println("git test 5");
+		System.out.println("git test 6");
+
+	}
+
+}
