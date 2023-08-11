08477ec16cd4d516776f1411b2b2c7861f134088
Ayman Mreiden <amoryden@gmail.com>
2023-08-11 17:01:41 -0500

    commited

--- src/main/java/hmwsession4/GitTest2.java
+++ src/main/java/hmwsession4/GitTest2.java
@@ -13,8 +13,8 @@ public class GitTest2 {
 
 		System.out.println("git test 1");
 		System.out.println("git test 2");
-		System.out.println("git test 3");
-		System.out.println("git test 4");
+		System.out.println("git test 3.1");
+		System.out.println("git test 4.5");
 		System.out.println("git test 5");
 		System.out.println("git test 6");
 
